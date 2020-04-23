70b11884e4de45986d2e1e2f360e8803a6e801b0
Param <paramchauhan21@gmail.com>
2020-04-23 12:37:10 +0530

    modify settings.py for heroku

--- ays_basic/settings.py
+++ ays_basic/settings.py
@@ -173,3 +173,8 @@ STATICFILES_DIRS = (
     # Machina
     MACHINA_MAIN_STATIC_DIR,
 )
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
