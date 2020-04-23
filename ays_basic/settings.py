38bfa02d3837f4a73336aa58a87fba629ac0de49
Param <paramchauhan21@gmail.com>
2020-04-23 14:32:53 +0530

    Updated allowed hosts

--- ays_basic/settings.py
+++ ays_basic/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '1y1_#t$#xq$c6d^b^m-96y%vl*q2^=
 # DEBUG = False
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['peaceful-sands-50773.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
